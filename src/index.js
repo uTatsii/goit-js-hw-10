045f7c8ad2417da9573ab6a2f3671a473ab9a239
uTatsii <utatsiy@gmail.com>
2022-09-19 17:38:32 +0300

    added fetchCountries function

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import './css/styles.css';
-import fetchCountries from './fetchCountries';
+import './js/fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
---
+++ src/js/fetchCountries.js
@@ -0,0 +1,11 @@
+const URL = 'https://restcountries.com/v3.1/name/';
+const FILTER = '?fields=name,capital,population,flags,languages';
+
+export function fetchCountries(name) {
+    return fetch(URL + name + FILTER).then((response) => {
+        if (!response.ok) {
+            throw new Error(response.status);
+        }
+        return response.json();
+    })
+}
